d5771e29cfbdfc01fb486e97e40ee54593df96d2
tinabenita <tina1717rego@gmail.com>
2023-05-31 22:07:46 +0530

    cat driving me mad :)

--- cat.c
+++ cat.c
@@ -1,10 +1,3 @@
-/* Command to run cat.c:
-
-gcc -o main cat.c -lGL -lGLU -lglut -lm
-./main
-
-*/
-
 #include <GL/glut.h>
 #include <math.h>
 
@@ -16,60 +9,20 @@ void init() {
     glLoadIdentity();
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 }
-
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-
-    // Rainbow tail of the cat
+    
     float exitxL = -0.5;  // Example value for exitxL
     float exityL = 0.5;   // Example value for exityL
     float exityU = -0.5;  // Example value for exityU
     
-    float dx = exitxL;
-    float dy = exityL;
+    float bx = exitxL * 0.5;  // Half the length along the x-axis
+    float by = exityL;
     float w = 0.014;
     float step = (exityL - exityU) / 5.0;
-  
-
-    for (int i = 0; i < 50; i++) {
-        glBegin(GL_QUAD_STRIP);
-
-        glColor3f(1, 0, 0);
-        glVertex2f(dx, dy);
-        glVertex2f(dx + w, dy);
-
-        glColor3f(1, 0.6, 0);
-        glVertex2f(dx, dy - step);
-        glVertex2f(dx + w, dy - step);
-
-        glColor3f(1, 1, 0);
-        glVertex2f(dx, dy - step * 2);
-        glVertex2f(dx + w, dy - step * 2);
-
-        glColor3f(0.2, 1, 0);
-        glVertex2f(dx, dy - step * 3);
-        glVertex2f(dx + w, dy - step * 3);
-
-        glColor3f(0, 0.6, 1);
-        glVertex2f(dx, dy - step * 4);
-        glVertex2f(dx + w, dy - step * 4);
-
-        glColor3f(0.4, 0.2, 1);
-        glVertex2f(dx, dy - step * 5);
-        glVertex2f(dx + w, dy - step * 5);
-
-        glEnd();
-
-        dx += w;
-        dy += 0.01 * sin(i * 3.14 / 5.0);
-    }
-
-    // Paws and tail
-    float bx = dx;
-    float by = dy;
     float bh = step * 5.0;
-    float bw = bh * 0.80952;
+    float bw = bh * 0.80952 * 0.5;  // Half the length along the x-axis
     float round = 0.024;
 
     float th = 0.05;
@@ -93,12 +46,41 @@ void display() {
     glVertex2f(bx - ts * 4, ty);
     glVertex2f(bx - ts * 4, ty + th);
     glEnd();
+    
+   // Dark grey color for the entire cat shape
+	glBegin(GL_POLYGON);
+	glColor3f(0.2, 0.2, 0.2);  // Dark grey color
+	glVertex2f(bx, by-0.75);
+	glVertex2f(bx-0.5 + round, by + round-0.75);
+	glVertex2f(bx- round + bw, by + round-0.75);
+	glVertex2f(bx+ bw, by-0.75);
+	glVertex2f(bx + bw, (by - bh / 4)-0.75 );  // Reduce the height to half (divide by 2)
+	glVertex2f(bx -0.75+ bw - round, (by - bh / 4 - round)-0.75 );
+	glVertex2f(bx + round, (by - bh / 4 - round)-0.75 );
+	glVertex2f(bx, (by - bh / 4)-0.75 );
+	glEnd();
+
+	// Light grey color for the inner rectangular polygon with rounded edges
+	glBegin(GL_POLYGON);
+	glColor3f(0.35, 0.35, 0.35); // Light grey color
+	float innerRound = round * 0.8;  // Adjust the inner round value to make it smaller than the outer round value
+	glVertex2f(bx + round, by - 0.75);
+	glVertex2f(bx + round, (by - bh / 4 - round) - 0.75);
+	glVertex2f(bx + bw - round, (by - bh / 4 - round) - 0.75);
+	glVertex2f(bx + bw - round, by - 0.75);
+	glVertex2f(bx + bw - innerRound, by - 0.75);
+	glVertex2f(bx + bw - innerRound, (by - bh / 4 - innerRound) - 0.75);
+	glVertex2f(bx + innerRound, (by - bh / 4 - innerRound) - 0.75);
+	glVertex2f(bx + innerRound, by - 0.75);
+	glEnd();
+
     // Four paws
     float fr = 0.02;
     float fx = bx + bw;
     float fy = by - bh - fr;
 
     glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.6, 0.6, 0.6);
     for (int i = 0; i <= 200; i += 6) {
         glVertex2f(fx + fr * cos(i), fy + fr * sin(i));
     }
@@ -106,6 +88,7 @@ void display() {
 
     fx -= bw;
     glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.6, 0.6, 0.6);
     for (int i = 0; i <= 200; i += 6) {
         glVertex2f(fx + fr * cos(i), fy + fr * sin(i));
     }
@@ -113,6 +96,7 @@ void display() {
 
     fx = fx + bw * 0.8;
     glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.6, 0.6, 0.6);
     for (int i = 0; i <= 200; i += 6) {
         glVertex2f(fx + fr * cos(i), fy + fr * sin(i));
     }
@@ -120,68 +104,15 @@ void display() {
 
     fx = fx * 0.6;
     glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.6, 0.6, 0.6);
     for (int i = 0; i <= 200; i += 6) {
         glVertex2f(fx + fr * cos(i), fy + fr * sin(i));
     }
     glEnd();
-
-    // Sparkly body
-    glBegin(GL_POLYGON);
-    glColor3f(0, 0, 0);
-    glVertex2f(bx, by);
-    glVertex2f(bx + round, by + round);
-    glVertex2f(bx - round + bw, by + round);
-    glVertex2f(bx + bw, by);
-    glVertex2f(bx + bw, by - bh);
-    glVertex2f(bx + bw - round, by - bh - round);
-    glVertex2f(bx + round, by - bh - round);
-    glVertex2f(bx, by - bh);
-    glEnd();
-
-    // Crust of pop tart body
-    float scale = 0.95;
-    round *= scale;
-    bx += (bw - bw * scale) / 2.0;
-    by -= (bh - bh * scale) / 2.0;
-    bh *= scale;
-    bw *= scale;
-
-    glBegin(GL_POLYGON);
-    glColor3f(1, 0.8, 0.6);
-    glVertex2f(bx, by);
-    glVertex2f(bx + round, by + round);
-    glVertex2f(bx - round + bw, by + round);
-    glVertex2f(bx + bw, by);
-    glVertex2f(bx + bw, by - bh);
-    glVertex2f(bx + bw - round, by - bh - round);
-    glVertex2f(bx + round, by - bh - round);
-    glVertex2f(bx, by - bh);
-    glEnd();
-
-    // Frosting
-    scale = 0.86;
-    round *= scale;
-    bx += (bw - bw * scale) / 2.0;
-    by -= (bh - bh * scale) / 2.0;
-    bh *= scale;
-    bw *= scale;
-
-    glBegin(GL_POLYGON);
-    glColor3f(1, 0.6, 1);
-    glVertex2f(bx, by);
-    glVertex2f(bx + round, by + round);
-    glVertex2f(bx - round + bw, by + round);
-    glVertex2f(bx + bw, by);
-    glVertex2f(bx + bw, by - bh);
-    glVertex2f(bx + bw - round, by - bh - round);
-    glVertex2f    (bx + round, by - bh - round);
-    glVertex2f(bx + round, by - bh - round);
-    glVertex2f(bx, by - bh);
-    glEnd();
-
+    
     // Face
-     fx = bx + bw;
-     fy = by - bh * 0.8;
+    fx = bx + bw;
+    fy = by - bh * 0.8;
     fr = 0.07;
 
     glColor3f(0.6, 0.6, 0.6);
@@ -259,7 +190,7 @@ void display() {
     }
     glEnd();
 
-        // Mouth
+    // Mouth
     fy *= 0.8;
     float mw = 5;  // Mouth width measured in nose widths
     float mh = 2;  // Mouth height measured in nose widths
@@ -291,11 +222,12 @@ void display() {
 
     glFlush();
 }
+
 int main(int argc, char **argv) {
     // Initialize GLUT and create a window
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(1500, 1000);
+    glutInitWindowSize(800, 600);
     glutCreateWindow("Cat-astrophe Escape");
 
     // Set the display function
@@ -309,5 +241,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
 
